b663fd3029557f3bb784ee86e133cf1d8e4941b1
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2023-10-22 18:08:29 +0600

    deleted navbar comment

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,6 @@ const Navbar = () => {
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={24}/>  {/* Work */}
-        <Image src="../public/hilink-logo.svg" alt="logo" width={74} height={24}/>  {/* Doesn't Work */}
 
       </Link>
         <ul className="hidden h-full gap-12 lg:flex">
